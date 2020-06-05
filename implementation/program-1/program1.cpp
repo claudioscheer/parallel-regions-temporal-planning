9e5b1553d24d30ad5aa6bbc954931b996ef9a399
Claudio Scheer <claudioscheer@protonmail.com>
2020-06-05 16:43:14 -0300

    Update program-1

--- implementation/program-1/program1.cpp
+++ implementation/program-1/program1.cpp
@@ -2,10 +2,8 @@
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
     int a = 3;
     int b = 3;
-    std::cout << "Result: ";
     int c = a + b;
     std::cout << c << std::endl;
     return 0;
