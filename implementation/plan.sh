30ad6307186a0a475463536ac9b44c83b782c9d3
Claudio Scheer <claudioscheer@protonmail.com>
2020-06-19 11:25:28 -0300

    Remove compiler-domain-example

--- implementation/plan.sh
+++ implementation/plan.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 rm -f dom.pddl ins.pddl output output.sas tdom.pddl tins.pddl sas_plan.* tmp_sas_plan.*
-python2 ../../planner/temporal-planning/bin/plan.py $1 domain.pddl $2 --no-validate --time 60 --no-iterated
+python2 ../planner/temporal-planning/bin/plan.py $1 domain.pddl $2 --no-validate --time 60 --no-iterated
 rm -f dom.pddl ins.pddl output output.sas tdom.pddl tins.pddl #sas_plan.* tmp_sas_plan.*
